620350a671f44fca1c9293fa4b815d0fe0632f41
Ilkka Törmä <iatorm@utu.fi>
2015-04-17 18:16:39 +0300

    Added all files.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# shift
-Shift: a stack-based functional esoteric programming language.
+# Shift
+
+_Shift_ is an esoteric functional programming language.
+It is stack-based, but also has automatic currying like Haskell.
+
+# Specification
+
+There are two datatypes in Shift:
+
+- Functions, which can have an arbitrary positive _arity_ (number of inputs), and which return a list of outputs. For example, a function that duplicates its only input has arity 1, and a function that swaps its two inputs has arity 2.
+- Blanks, which are all identical and have no other purpose than not being functions.
+
+A Shift program consists of zero or more _commands_, each of which is a single ASCII character.
+There are 8 commands in total:
+
+- `!` (_apply_) pops a function `f` and a value `x` from the stack, and applies `f` to `x`. If `f` has arity 1, the list `f(x)` is appended to the stack. If it has arity `n > 1`, a new `(n-1)`-ary function `g` is pushed to the stack. It takes inputs <code>x<sub>1</sub>,x<sub>2</sub>,...,x<sub>n-1</sub></code> and returns <code>f(x,x<sub>1</sub>,x<sub>2</sub>,...,x<sub>n-1</sub>)</code>.
+- `?` (_blank_) pushes a blank to the stack.
+- `+` (_clone_) pushes to the stack a unary function that duplicates its input: any value `x` is mapped to `[x,x]`.
+- `>` (_shift_) pushes to the stack a unary function that takes in an `n`-ary function `f`, and returns an `(n+1)`-ary function `g` that ignores its first argument, and calls `f` on the remaining ones.
+- `/` (_fork_) pushes to the stack a ternary function that takes three inputs `a,b,c`, and returns `[b]` if `a` is a blank, and `[c]` otherwise.
+- `$` (_call_) pushes to the stack a binary function that pops a function `f` and a value `x`, and applies `f` to `x` exactly as `!` does.
+- `.` (_chain_) pushes to the stack a binary function that pops two functions `f` and `g`, and returns their composition: a function `h` that has the same arity as `f`, and which takes its inputs normally, applies `f` to them, and then _fully_ applies `g` to the result.
+- `@` (_say_) pushes a unary function that simply returns its input, and prints `0` if it was a blank, and `1` if it was a function.
+
+Note that all commands except `!` simply push a value to the stack, there is no way to perform input, and the only way to output anything is to use `@`.
+A program is interpreted by evaluating the commands one by one, printing `0`s or `1`s whenever "say" is called, and exiting.
+Any behavior not described here (applying a blank, applying a stack of length 0 or 1, calling "chain" on a blank etc.) is undefined.
+
+# This Interpreter
+
+The reference interpreter, written in Haskell, has the following extra features:
+
+- Informative error messages.
+- All functions can be referred to by their lowercase names, instead of the symbols. They must be separated by symbols or whitespace.
+- All whitespace is ignored, except between lowercase words, where it acts as a separator. An uppercase letter begins a comment that extends to the end of the line.
---
+++ shift.hs
@@ -0,0 +1,98 @@
+import System.Environment (getArgs)
+import Data.Char (isUpper)
+import Control.Monad (liftM, (>=>))
+import Control.Monad.Writer.Lazy (Writer, writer, tell, execWriter)
+import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
+
+type Out a = MaybeT (Writer String) a
+
+exitWith :: String -> Out a
+exitWith s = tell s >> fail ""
+
+data Cmd = I Item
+         | Apply
+
+instance Show Cmd where
+  show (I i) = "C" ++ show i
+  show Apply = "!"
+
+data Item = Blank
+          | Funct {arity :: Int,
+                   rule :: [Item] -> Out [Item]}
+
+instance Show Item where
+  show Blank = "?"
+  show (Funct n _) = show n ++ "F"
+
+apply :: Cmd -> [Item] -> Out [Item]
+apply Apply (Funct 1 f : i : is) = liftM (++is) $ f [i]
+apply Apply (Funct n f : i : is) = return $ (Funct (n-1) $ f . (i:)) : is
+apply Apply (Blank : _) = exitWith $ "Error: tried to apply blank"
+apply Apply [Funct n f] = exitWith $ "Error: tried to apply " ++ show n ++ "-ary function to empty list"
+apply Apply [] = exitWith "Error: tried to apply empty list"
+apply (I i) is = return $ i : is
+
+parse :: String -> [Cmd]
+parse = map parse' 
+        . concatMap separate 
+        . concatMap (words . takeWhile (not . isUpper)) 
+        . lines
+  where separate "" = []
+        separate s@(c:str) = 
+          if c `elem` symbols
+          then [c] : separate str
+          else wd : separate rest
+            where (wd,rest) = span (not . (`elem` symbols)) s
+        symbols = "!?+>/$.@"
+        parse' "!" = Apply
+        parse' "?" = I Blank
+        parse' "clone" = clone
+        parse' "+" = clone
+        parse' "shift" = shift
+        parse' ">" = shift
+        parse' "fork" = fork
+        parse' "/" = fork
+        parse' "call" = call
+        parse' "$" = call
+        parse' "chain" = chain
+        parse' "." = chain
+        parse' "say" = say
+        parse' "@" = say
+
+interpret :: String -> String
+interpret = execWriter . runMaybeT . ($ []) . foldr1 (>=>) . map apply . parse
+
+main :: IO ()
+main = do
+  [file] <- getArgs
+  prog <- readFile file
+  putStrLn $ interpret prog
+
+clone = let f (i:is) = return $ i:i:is
+            f [] = exitWith "Error: tried to apply clone to empty list"
+        in I $ Funct 1 f
+
+shift = let f (Funct n g:is) = return $ Funct (n+1) h : is
+              where h (x:xs) = liftM (x:) $ g xs
+                    h [] = exitWith $ "Error: tried to apply shifted " ++ show (n+1) ++ "-ary function to empty list"
+            f is@(Blank:_) = exitWith "Error: tried to apply shift to blank"
+            f [] = exitWith "Error: tried to apply shift to empty list"
+        in I $ Funct 1 f
+
+fork = let f (Blank:i:_:is) = return $ i : is
+           f (_:_:is) = return is
+           f is = exitWith $ "Error: tried to apply fork to " ++ show (length is) ++ "-element list"
+       in I $ Funct 3 f
+
+call = I $ Funct 1 $ apply Apply
+
+chain = let f ((Funct n g):(Funct m h):is) =
+              return $ (Funct m $ h >=> g):is
+            f (_:_:is) = exitWith "Error: tried to apply chain to blank"
+            f is = exitWith $ "Error: tried to apply fork to " ++ show (length is) ++ "-element list"
+        in I $ Funct 2 f
+
+say = let f is@(Blank:_) = writer (is,"0")
+          f is@(_:_) = writer (is,"1")
+          f is = exitWith "Error: tried to apply say to empty list"
+      in I $ Funct 1 f
